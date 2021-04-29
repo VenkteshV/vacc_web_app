726a6bea2a591fb00146c9f0f16cfffca544f130
VenkteshV <venkkratos@gmail.com>
2021-04-30 03:11:17 +0530

    added all pages components

--- client/components/FAQ.jsx
+++ client/components/FAQ.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import FcContainer from '../containers/FcContainer.jsx'
 import * as faqs from '../constants/faqs';
+import {Panel} from 'react-bootstrap';
+import _ from 'lodash';
 export default class FAQ extends React.Component {
   constructor(props) {
     super(props);
@@ -15,13 +17,50 @@ export default class FAQ extends React.Component {
       console.log("faqs",faqs);
     return (
 <div>
-    {
-        faqs.faqs.map(faq => {
-            let answer = this.renderHtml(faq.answer);
-            return (<div><p className="Questions">{faq.question}</p> <p className="Answer" dangerouslySetInnerHTML={{
-                __html: answer}}></p> </div>)
+<div className="FaqCard">
+  <p className="PledgeQuestion">What is the Oosi Podu Pledge? </p> 
+
+   <p className = "PledgeAnswer"> While several of us are in the process or have registered to get vaccinations, there are still a lot of people who are thinking twice about getting vaccinated. By taking this simple pledge and sharing with your friends, you are helping build a conversation around getting vaccinated and encouraging more people to get vaccinated.
+   <ol><li>I will get vaccinated or I have been vaccinated.</li>
+   <li> I will encourage others to get vaccinated.</li>
+   <li>I will verify any information around vaccinations before sharing.</li>
+   </ol> </p>
+   <p className="PledgeQuestion"> We already know we need to get vaccinated, so why this pledge?  </p> 
+
+<p className = "PledgeAnswer"> While several of us are in the process or have registered to get vaccinations, there are still a lot of people who are thinking twice about getting vaccinated. By taking this simple pledge and sharing with your friends, you are helping build a conversation around getting vaccinated and encouraging more people to get vaccinated.
+While we are already hearing this from our government, health experts, and news media... the Oosi Podu pledge hopes to use the power of friendship and social media to help reach 40 Lakh vaccinations in our city. 
+    </p>
+   </div>
+
+<div className="Panel">
+  <Panel className = "CollapsablePanel">
+  <Panel.Heading>
+          <Panel.Title>
+           <div className="QuestionBlock"> <p className = "Header">Below are FAQs around getting the vaccinations</p></div>
+          </Panel.Title>
+        </Panel.Heading>
+  </Panel>
+  
+    { faqs.faqs.map((faq,index) => {
+        let answer = this.renderHtml(faq.answer);
+        return (
+        <Panel className = "CollapsablePanel">
+        <Panel.Heading>
+          <Panel.Title toggle>
+           <div className="QuestionBlock"><p className="Questions">{faq.question}</p> <i className ="fa fa-angle-down"></i> </div>
+          </Panel.Title>
+        </Panel.Heading>
+        <Panel.Collapse>
+          <Panel.Body>
+          <p className="Answer" dangerouslySetInnerHTML={{
+                __html: answer}}></p>
+          </Panel.Body>
+        </Panel.Collapse>
+        </Panel> )
+
         })
     }
+</div>
 </div>
     );
 }
--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -1,69 +1,134 @@
 import React, { Component } from "react";
 import Progress from "./Progress.jsx";
 import Logo from "../images/baseline-check_circle_outline-24px.svg";
-import VaccineImg from "../images/poster.png";
+import VaccineImg from "../images/Banner.png";
+import ChennaiTricolor from '../images/chennai_tricolor.png';
 import { Button, ProgressBar, Tabs, Tab } from 'react-bootstrap';
 import FAQ from './FAQ.jsx';
 import _ from 'lodash';
 import Select from 'react-select';
 import PropTypes from 'prop-types';
-
+import CancelPage from './CancelPage.jsx';
+import PledgePage from './PledgePage.jsx';
 
 class Landing extends Component {
   constructor(props) {
     super(props);
     this.state = {
       progress: 20,
-      numberVaccinated:1000000,
-      shouldRenderFaq: false
+      milsestones: [
+        { 'number': 10000, 'text': '10,000' },
+        { 'number': 25000, 'text': '25,000' },
+        { 'number': 50000, 'text': '50,000' },
+        { 'number': 100000, 'text': '1,00,000' },
+        { 'number': 250000, 'text': '2,50,000' },
+        { 'number': 500000, 'text': '5,00,000' },
+        { 'number': 750000, 'text': '7,50,000' },
+        { 'number': 1000000, 'text': '10,00,000' },
+        { 'number': 2500000, 'text': '25,00,000' },
+        { 'number': 4000000, 'text': '40,00,000' }],
+      milestoneCount: { 'number': 5000, 'text': '5,000' },
+      numberVaccinated: { 'number': 10000, 'text': '10,000' },
+      shouldRenderFaq: false,
+      shouldRenderCancel: false,
+      shouldRenderPledgeForm: false
     };
     this.renderFaq = this.renderFaq.bind(this);
+    this.toggleFaq = this.toggleFaq.bind(this);
+    this.renderCancel = this.renderCancel.bind(this);
+    this.toggleCancel = this.toggleCancel.bind(this);
+    this.renderPledgeForm = this.renderPledgeForm.bind(this);
+    this.togglePledge = this.togglePledge.bind(this);
   }
 
 
- 
 
 
 
+  renderCancel() {
+    this.setState({ shouldRenderCancel: true });
+  }
+
+  renderPledgeForm() {
+    this.setState({ shouldRenderPledgeForm: true });
+
+  }
+  toggleCancel() {
+    this.setState({ shouldRenderCancel: false });
+
+  }
+  togglePledge() {
+    this.setState({shouldRenderPledgeForm:false})
+  }
 
   renderProgress() {
-    const totalCount=4000000;
-    const uploadProgress = this.state.numberVaccinated;
-    const nowProgress = (uploadProgress/totalCount)*100;
+    if (this.state.numberVaccinated.number > this.state.milestoneCount.number) {
+      let newMilestone = this.state.milsestones.find((mile) => {
+        if (this.state.numberVaccinated.number < mile.number) {
+          return mile
+        }
+      })
+      this.setState({ milestoneCount: newMilestone })
+    }
+    const totalCount = this.state.milestoneCount.number;
+    console.log("totalCount", totalCount)
+    const uploadProgress = this.state.numberVaccinated.number;
+    const nowProgress = (uploadProgress / totalCount) * 100;
+    if (nowProgress == 100) {
+      let nextMilestone = _.clone(this.state.milsestones);
+      let newMilestone = nextMilestone.find((mile) => {
+        if (uploadProgress < mile.number) {
+          return mile
+        }
+      });
+      console.log("newMilestone", newMilestone)
+      let deleted = nextMilestone.splice(0, _.findIndex(nextMilestone, newMilestone))
+      this.setState({ milestoneCount: newMilestone, milsestones: nextMilestone })
+    }
 
-      return (
-        <div>
-            <ProgressBar variant="success" now={nowProgress} label= {`${nowProgress}%`} />
-            </div>
+    return (
+      <div>
+        <ProgressBar variant="success" now={nowProgress} label={`${nowProgress}%`} />
+      </div>
 
-      );
+    );
   }
 
   renderFaq(key) {
-    if (key=="faq") {
-    console.log("here")
-    this.setState({shouldRenderFaq: !this.state.shouldRenderFaq});
-  }
-}
-  renderActions() {
-      return (
-        <Button
-        className="Register"
-          onClick={this.uploadFiles}
-        >
-          Take the vaccination pledge
-        </Button>
-      );
-    }
+    if (key == "faq") {
+      console.log("here")
+      this.setState({ shouldRenderFaq: !this.state.shouldRenderFaq });
+      this.setState({ shouldRenderCancel: false });
+      this.setState({ shouldRenderPledgeForm: false });
 
- 
+
+    }
+  }
+
+  toggleFaq() {
+    this.setState({ shouldRenderFaq: !this.state.shouldRenderFaq });
+
+  }
+  renderActions() {
+    return (
+      <Button
+        className="Register"
+        onClick={this.renderPledgeForm}
+      >
+        <div className="PledgeButton"> Take the Oosi Podu Pledge </div>
+      </Button>
+    );
+  }
+
+
 
   render() {
-    console.log("renderfaq",this.state.shouldRenderFaq);
+    const FooterClassName = this.state.shouldRenderCancel ? 'FooterTabs': 'Footer';
+    console.log("renderfaq", this.state.shouldRenderFaq);
     return (
       <div>
 
-      <div className = "card_lo"> Chennai Vaccination Pledge
+
 
         {/* {!_.isEmpty(this.state.learningObj["rankedlo"]) ? (
           <div>
@@ -76,36 +141,50 @@ class Landing extends Component {
                 </li>
               ))}
             </ul> </div>) : (null)} */}
-      </div>
-      { this.state.shouldRenderFaq ?(       <div className="FaqCard">
-  {/* <div className="Content">
-  </div> */}
-  <FAQ/>
-</div>) : ( <div>
-      <img className="VaccineImg" src={VaccineImg}/>
-      <div><span className="Title">Chennai vaccination drive</span></div>
-      <div><span className="Verbiage"> We are on a mission to vaccinate 40 Lakh people to break the chain. If you have registered to take the vaccination, plan to register or if you have gotten your vaccination click below to inspire others as well to get vaccinated. </span></div>
-      <div className="VaccineCard">
-        <div className="Card">
-
-          <div className="Upload">
+        {this.state.shouldRenderPledgeForm ? (<div> <div className="card_lo">
+            <i onClick={this.togglePledge} className="fa fa-arrow-left"></i>
+            <span className="faq"> The Oosi Podu Pledge</span> </div>
+            <PledgePage/>
+          </div>) : (this.state.shouldRenderCancel ? (
+          <div> <div className="card_lo">
+            <i onClick={this.toggleCancel} className="fa fa-arrow-left"></i>
+            <span className="faq"> Help us with the reason</span> </div>
+            <CancelPage/>
+          </div>) : (this.state.shouldRenderFaq ? (<div> <div className="card_lo"> <i onClick={this.toggleFaq} className="fa fa-arrow-left"></i> <span className="faq"> FAQ</span> </div>
             {/* <div className="Content">
-            </div> */}
-            <div><span className="Completed">{this.state.numberVaccinated} have signed.</span> <span className="Verbiage">Let's get to 40,00,000</span></div>
+  </div> */}
+            <FAQ />
+          </div>) : (<div> <div className="card_lo"> Oosi Podu </div> <div>
+            <img className="VaccineImg" src={VaccineImg} />
 
-            <div className="ProgressIndicator">{this.renderProgress()}</div>
-            <div className="Actions">{this.renderActions()}</div>
-            <div className="Actions"><Button className="Cancel" size="lg"> Not at the moment</Button></div>
+            <div className="VaccineCard">
+              <div className="Card">
+
+                <div className="Upload">
+                  <div><span className="Completed">{this.state.numberVaccinated.text} have signed.</span> <span className="Verbiage">Let's get to {this.state.milestoneCount.text}!</span></div>
+
+                  <div className="ProgressIndicator">{this.renderProgress()}</div>
+                  <div><span className="Completed"> Someone signed two minutes ago </span> </div>
+                  <div><span className="Verbiage Mission"> We are on a mission to vaccinate 40 Lakh people to break the chain. If you have registered to get vaccinated, plan to register or if you have gotten your vaccination, click below to inspire others as well to get vaccinated. </span></div>
+                  <div className="Actions">{this.renderActions()}</div>
+                  <div className="Cancel" onClick={this.renderCancel}>  Not at the moment</div>
+                </div>
+              </div> </div>  </div> </div>)))}
+        <div className={`${FooterClassName}`}>
+          <Tabs defaultActiveKey="profile" className="FaqTabs" id="uncontrolled-tab-example" onSelect={this.renderFaq}>
+            <Tab eventKey="faq" title="FAQ" className="Faq" >
+            </Tab>
+            {/* <Tab eventKey="Privacy policy" className="Privacy">
+  </Tab> */}
+          </Tabs>
+          <div className="TricolorImage">
+            <img className="ChennaiTricolor" src={ChennaiTricolor}></img>
           </div>
-         </div> </div>  </div>) }
-         <Tabs defaultActiveKey="profile" className="FaqTabs" id="uncontrolled-tab-example" onSelect={this.renderFaq}>
-  <Tab eventKey="faq" title="FAQ" className="Faq" >
-  </Tab>
-  <Tab eventKey="Privacy policy" title="Privacy policy" className="Privacy">
-  </Tab>
-</Tabs>  
+        </div>
       </div>
-      ); } }
+    );
+  }
+}
 Landing.propTypes = {
   fetchBloomVerbs: PropTypes.func,
 };
