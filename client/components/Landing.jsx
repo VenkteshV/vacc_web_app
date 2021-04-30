1367d9c603466ef6e1f68da64bf4e57df0621b71
Sachin Narula <sachin.narula@falcon.io>
2021-04-30 18:53:57 +0530

    [UPDATE][SVG] Replace fa-arrow-left icon with SVG

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -151,16 +151,25 @@ class Landing extends Component {
                 </li>
               ))}
             </ul> </div>) : (null)} */}
-        {this.props.renderNegBadge ? (<img className="VaccineImg" src={VisitAgainImg}></img>) :(this.state.shouldRenderPledgeForm ? (<div> <div className="card_lo">
-            <i onClick={this.togglePledge} className="fa fa-arrow-left"></i>
+        {this.props.renderNegBadge ? (<img className="VaccineImg" src={VisitAgainImg}></img>) :(this.state.shouldRenderPledgeForm ? (<div> <div className="card_lo">            
+            <svg onClick={this.togglePledge} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M20 10C20 9.30964 19.4404 8.75 18.75 8.75H4.81263L10.9062 2.6564C11.4043 2.15835 11.3971 1.34869 10.8904 0.859505C10.3961 0.38238 9.61069 0.389309 9.12492 0.87508L0.0990063 9.90099C0.0443264 9.95567 0.0443268 10.0443 0.0990067 10.099L9.12306 19.1231C9.60738 19.6074 10.3926 19.6074 10.8769 19.123C11.3596 18.6403 11.3614 17.8581 10.8808 17.3732L4.81263 11.25H18.75C19.4404 11.25 20 10.6904 20 10Z" fill="white"/>
+            </svg>
+
             <span className="u-position--center z-index"> The Oosi Podu Pledge</span> </div>
             <PledgePage persistData ={this.props.persistData}/>
           </div>) : (this.state.shouldRenderCancel ? (
-          <div> <div className="card_lo">
-            <i onClick={this.toggleCancel} className="fa fa-arrow-left"></i>
+          <div> <div className="card_lo">            
+            <svg onClick={this.toggleCancel} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M20 10C20 9.30964 19.4404 8.75 18.75 8.75H4.81263L10.9062 2.6564C11.4043 2.15835 11.3971 1.34869 10.8904 0.859505C10.3961 0.38238 9.61069 0.389309 9.12492 0.87508L0.0990063 9.90099C0.0443264 9.95567 0.0443268 10.0443 0.0990067 10.099L9.12306 19.1231C9.60738 19.6074 10.3926 19.6074 10.8769 19.123C11.3596 18.6403 11.3614 17.8581 10.8808 17.3732L4.81263 11.25H18.75C19.4404 11.25 20 10.6904 20 10Z" fill="white"/>
+            </svg>
             <span className="u-position--center z-index"> Help us with the reason</span> </div>
             <CancelPage triggerExcuse={this.props.triggerExcuse}/>
-          </div>) : (this.state.shouldRenderFaq ? (<div> <div className="card_lo"> <i onClick={this.toggleFaq} className="fa fa-arrow-left"></i> <span className="faq u-position--center z-index"> FAQ</span> </div>
+          </div>) : (this.state.shouldRenderFaq ? (<div> <div className="card_lo">             
+             <svg onClick={this.toggleFaq} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M20 10C20 9.30964 19.4404 8.75 18.75 8.75H4.81263L10.9062 2.6564C11.4043 2.15835 11.3971 1.34869 10.8904 0.859505C10.3961 0.38238 9.61069 0.389309 9.12492 0.87508L0.0990063 9.90099C0.0443264 9.95567 0.0443268 10.0443 0.0990067 10.099L9.12306 19.1231C9.60738 19.6074 10.3926 19.6074 10.8769 19.123C11.3596 18.6403 11.3614 17.8581 10.8808 17.3732L4.81263 11.25H18.75C19.4404 11.25 20 10.6904 20 10Z" fill="white"/>
+            </svg>
+             <span className="faq u-position--center z-index"> FAQ</span> </div>
             {/* <div className="Content">
   </div> */}
             <FAQ />
