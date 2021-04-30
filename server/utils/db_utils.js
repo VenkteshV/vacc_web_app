3b4759025d7b2c23e32143c87f5b433d07a9753d
siddharth-7316 <siddharth.na@zohocorp.com>
2021-04-30 16:44:12 +0530

    env conf for db

--- server/utils/db_utils.js
+++ server/utils/db_utils.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'oosipodudb',
-  password: 'postgres',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 })
 
 /*
